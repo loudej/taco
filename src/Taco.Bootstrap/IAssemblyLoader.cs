0acc9233b85371cbf3088bdb0c8f0cfcd60314db
Louis DeJardin <louis.dejardin@gmail.com>
2011-01-29 21:56:21 -0800

    Removing unused files

--- src/Taco.Bootstrap/IAssemblyLoader.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using Gate.Bootstrap;
-
-namespace Gate.Framework.BuilderServices {
-    public interface IAssemblyLoader {
-        IEnumerable<BuilderAttribute> Load(string name);
-    }
-}
