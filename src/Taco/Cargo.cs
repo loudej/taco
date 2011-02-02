6fc956a1c2c1b72e5b8f309e38a7e348db74036e
Louis DeJardin <louis.dejardin@gmail.com>
2011-02-01 19:41:30 -0800

    Extracting a base class

--- src/Taco/Cargo.cs
+++ src/Taco/Cargo.cs
@@ -1,23 +1,50 @@
 ﻿using System;
-using System.Threading.Tasks;
 
 namespace Taco {
-    public class Cargo<T> {
-        readonly T _result;
+
+    public class Cargo {
         readonly Delivery _delivery;
+        
+        protected Cargo() {
+        }
+
+        protected Cargo(Delivery delivery) {
+            _delivery = delivery;
+        }
+        protected Cargo(Cargo cargo) {
+            _delivery = cargo._delivery;
+        }
+
+        public bool Delayable { get { return _delivery != null; } }
+        public bool Delayed { get { return _delivery != null && _delivery.Delayed; } }
+        public void Delay() { _delivery.Delayed = true; }
+        public void Resume() { _delivery.Delayed = true; _delivery.Continuation(); }
+
+        protected class Delivery {
+            public Delivery(Action continuation) {
+                Continuation = continuation;
+            }
+
+            public bool Delayed { get; set; }
+            public Action Continuation { get; private set; }
+        }
+    }
+
+    public class Cargo<T> : Cargo {
+        readonly T _result;
 
         public Cargo(T result) {
             _result = result;
         }
 
-        public Cargo(T result, Action continuation) {
+        public Cargo(T result, Action continuation)
+            : base(new Delivery(continuation)) {
             _result = result;
-            _delivery = new Delivery(continuation);
         }
 
-        Cargo(T result, Delivery delivery) {
+        Cargo(T result, Cargo cargo)
+            : base(cargo) {
             _result = result;
-            _delivery = delivery;
         }
 
         //public static implicit operator Cargo<T>(T result) {
@@ -25,40 +52,9 @@ namespace Taco {
         //}
 
         public T Result { get { return _result; } }
-        public bool Delayable { get { return _delivery != null; } }
-        public bool Delayed { get { return _delivery != null && _delivery.Delayed; } }
-
-        public void Delay() { _delivery.Delayed = true; }
-        public void Resume() { _delivery.Delayed = true; _delivery.Continuation(); }
 
         public Cargo<T2> Alter<T2>(T2 result) {
-            return new Cargo<T2>(result, _delivery);
-        }
-    }
-
-    class Delivery {
-        public Delivery(Action continuation) {
-            Continuation = continuation;
-        }
-
-        public bool Delayed { get; set; }
-        public Action Continuation { get; private set; }
-    }
-
-    public static class CargoExtensions {
-        public static bool InvokeAsync<T>(this Action<Cargo<T>> action, T result, Action continuation) {
-            var cargo = new Cargo<T>(result, continuation);
-            action(cargo);
-            return cargo.Delayed;
-        }
-        public static void InvokeSync<T>(this Action<Cargo<T>> action, T result) {
-            action(new Cargo<T>(result));
-        }
-
-        public static bool OnNextAsync<T>(this IObserver<Cargo<T>> observer, T result, Action continuation) {
-            var cargo = new Cargo<T>(result, continuation);
-            observer.OnNext(cargo);
-            return cargo.Delayed;
+            return new Cargo<T2>(result, this);
         }
     }
 }
---
+++ src/Taco/CargoExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Taco {
+    public static class CargoExtensions {
+        public static bool InvokeAsync<T>(this Action<Cargo<T>> action, T result, Action continuation) {
+            var cargo = new Cargo<T>(result, continuation);
+            action(cargo);
+            return cargo.Delayed;
+        }
+        public static void InvokeSync<T>(this Action<Cargo<T>> action, T result) {
+            action(new Cargo<T>(result));
+        }
+
+        public static bool OnNextAsync<T>(this IObserver<Cargo<T>> observer, T result, Action continuation) {
+            var cargo = new Cargo<T>(result, continuation);
+            observer.OnNext(cargo);
+            return cargo.Delayed;
+        }
+    }
+}
