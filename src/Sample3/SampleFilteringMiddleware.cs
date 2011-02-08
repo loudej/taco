6da1d536ba9a4acd220736e50bb3756a204c7bfb
Louis DeJardin <louis.dejardin@gmail.com>
2011-02-08 12:30:03 -0800

    Adding a Deflate example of a buffered and/or rewritten body

--- src/Sample3/SampleFilteringMiddleware.cs
+++ src/Sample3/SampleFilteringMiddleware.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Text;
 using Sample3;
 using Taco;
 using Taco.Helpers.Utils;
 
 [assembly: Builder("Uppercase", typeof(SampleFilteringMiddleware), "Uppercase")]
+[assembly: Builder("Deflate", typeof(SampleFilteringMiddleware), "Deflate")]
 
 namespace Sample3 {
     using AppAction = Action<
@@ -43,6 +47,67 @@ namespace Sample3 {
                 .Select(index => input.Array[input.Offset + index])
                 .Select(b => (b >= 'a' && b <= 'z') ? (byte)(b + 'A' - 'a') : b)
                 .ToArray();
+        }
+
+
+
+        public static AppAction Deflate(AppAction app) {
+            return (env, fault, result) => {
+                if (HasAcceptEncodingDeflate(env)) {
+                    app(env, fault, (status, headers, body) => {
+                        headers["Content-Encoding"] = "deflate";
+                        headers.Remove("Content-Length");
+                        result(status, headers, body.Filter((subscribe, next, error, complete) => {
+                            var bodyStream = new BodyStream(next, error, complete);
+                            var deflateStream = new DeflateStream(bodyStream, CompressionMode.Compress, false);
+                            return subscribe(data => Write(data, deflateStream), error, deflateStream.Close);
+                        }));
+                    });
+                }
+                else {
+                    app(env, fault, result);
+                }
+            };
+        }
+
+        private static bool HasAcceptEncodingDeflate(IDictionary<string, object> env) {
+            object acceptEncoding;
+            if (env.TryGetValue("HTTP_ACCEPT_ENCODING", out acceptEncoding) &&
+                Convert.ToString(acceptEncoding).Split(',').Contains("deflate")) {
+                return true;
+            }
+            return false;
+        }
+
+        private static void Write(Cargo<object> cargo, Stream stream) {
+            var data = Normalize(cargo.Result);
+
+            if (!cargo.Delayable) {
+                stream.Write(data.Array, data.Offset, data.Count);
+                return;
+            }
+
+            var result = stream.BeginWrite(data.Array, data.Offset, data.Count, asyncResult => {
+                if (asyncResult.CompletedSynchronously)
+                    return;
+                stream.EndWrite(asyncResult);
+                cargo.Resume();
+            }, null);
+
+            if (result.CompletedSynchronously)
+                stream.EndWrite(result);
+            else
+                cargo.Delay();
+        }
+
+        private static ArraySegment<byte> Normalize(object data) {
+            if (data is ArraySegment<byte>)
+                return (ArraySegment<byte>)data;
+            if (data is byte[])
+                return new ArraySegment<byte>((byte[])data);
+
+            //todo: have response encoding in environment?
+            return new ArraySegment<byte>(Encoding.Default.GetBytes(Convert.ToString(data)));
         }
     }
 }
